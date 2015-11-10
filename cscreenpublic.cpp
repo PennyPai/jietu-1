aa1f6020b80236f1afb1ccac551676175cc36a00
zhengwei <weinkym@qq.com>
2015-11-10 17:15:08 +0800

    调整命名规则

--- cscreenpublic.cpp
+++ cscreenpublic.cpp
@@ -1,6 +1,6 @@
 #include "cscreenpublic.h"
 
-QList<QRect> c_global_windown_rect_list; //用于存储所有的窗口
+QList<QRect> gWindownRectList; //用于存储所有的窗口
 #ifdef Q_OS_WIN
 #include <Windows.h>
 #include <vector>
@@ -17,9 +17,9 @@ bool CALLBACK getWindownRect(HWND hwnd,LPARAM lParam)
         rect.setTopLeft(QPoint(tempRect.left,tempRect.top));
         //QRect 差一个px
         rect.setBottomRight(QPoint(tempRect.right - 1,tempRect.bottom - 1));
-        if(!c_global_windown_rect_list.contains(rect))
+        if(!gWindownRectList.contains(rect))
         {
-            c_global_windown_rect_list.append(rect);
+            gWindownRectList.append(rect);
         }
         //遍历子窗口,TODO
         //  ::EnumChildWindows(hwnd,(WNDENUMPROC)updateWindowRectList,0);
@@ -30,8 +30,8 @@ bool CALLBACK getWindownRect(HWND hwnd,LPARAM lParam)
 
 QList<QRect> getWindownRectList()
 {
-    c_global_windown_rect_list.clear();
+    gWindownRectList.clear();
     //遍历所有顶级窗口,更新窗口大小列表
     ::EnumWindows((WNDENUMPROC)getWindownRect,0);
-    return c_global_windown_rect_list;
+    return gWindownRectList;
 }
